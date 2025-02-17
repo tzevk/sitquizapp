ddc78b11900942bb69b93d487935e5fb08ea63b5
Tanvi Kadam <91339637+tzevk@users.noreply.github.com>
2025-02-17 13:26:10 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 import { Inter } from "next/font/google";
-import './globals.css';
+import './global.css';
 import { Toaster } from "../components/ui/toaster";
 import axios from "axios";
 // axios.baseURL = "https://suvidya-chemtech-quiz.vercel.app";
